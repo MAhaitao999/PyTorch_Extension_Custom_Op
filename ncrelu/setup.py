27b93e28516dcede4d23f455e44a70da93db7504
Henry Ma <769413715@qq.com>
2022-05-04 16:03:02 +0800

    add some useful information in ncrelu/setup.py

--- ncrelu/setup.py
+++ ncrelu/setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
 setup(name='alpha_ncrelu',
+      version='0.1.0',
+      description='PTorch Custom defined Op: alphaNCReLU',
+      author='Henry Ma',
+      author_email='769413715@qq.com',
       ext_modules=[cpp_extension.CUDAExtension('alpha_ncrelu',
                       ['src/alpha_ncrelu.cc', 'src/alpha_ncrelu_kernel.cu'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
